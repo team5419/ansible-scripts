92ad5b489c20d73467fb65d80aec8fd155915ccb
Ezra Kainz <ezra.kainz@gmail.com>
2022-03-02 15:12:23 -0800

    Updated Scripts
    Working on fully working version

---
+++ pitcomputer/startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Set to either 'pit' or 'finder'
+MODE=PIT
+
+case MODE in
+
+  pit)
+    mpv /home/team/Desktop/compDisplay/
+    ;;
+
+  finder)
+    cd /home/team/Desktop/MAPSS/
+    python3 /home/team/Desktop/MAPSS/readSheet.py
+    sleep 3
+    firefox --kiosk 127.0.0.1:5000
+    ;;
+
+  *)
+    echo "mode must be either 'pit' or 'finder'"
+    ;;
+esac
+
